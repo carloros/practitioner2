4c56434cf218e5bdeb1db7733b49766b4bb2bbcd
Carlos <carlosj.fab@gmail.com>
2019-09-19 20:16:23 +0200

    WIP

--- hello-world.js
+++ hello-world.js
@@ -1,8 +1,10 @@
 // JS de tota la vida
-var greeting = 'Hola mon';
+// var greeting = 'Hola mon';
 
-// const greeting = 'Hola mon';
+const greeting = 'Hola mon';
 
 // let greeting = 'Hola mon';
+
+//Scope // Ambit
 
 console.log(greeting);
