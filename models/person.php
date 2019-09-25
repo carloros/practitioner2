00a26d5af4ff78edf37e9a1a479b1b7c976269c3
Carlos <carlosj.fab@gmail.com>
2019-09-25 17:36:36 +0200

    Objecte creat amb PHP

--- models/person.php
+++ models/person.php
@@ -5,3 +5,16 @@
             'surname' => 'Jimenez Fabregat',
             'email' => 'carlosj.fab@gmail.com',
         ];
+
+//$person = (object) [
+//    'fullname' => 'Sergi Tur Badenas',
+//    'name' => 'Sergi',
+//    'surname' => 'Tur Badenas',
+//    'email' => 'sergiturbadenas@gmail.com',
+//];
+
+$person1 = new stdClass();
+$person1->fullname = "Carlos Jimenez Fabregat";
+$person1->name = "Carlos";
+$person1->surname = "Jimenez Fabregat";
+$person1->email = "carlosj.fab@gmail.com";
