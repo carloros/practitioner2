09a617a0dbc7eb3d2c1e983984e2844ac4ec4035
Carloros <carlosj.fab@gmail.com>
2019-09-26 12:20:14 +0200

    Reparació connexio a la BDD

--- models/task.php
+++ models/task.php
@@ -6,11 +6,18 @@
 //     'completed' => true,
 // ];
 
-try {
-    $pdo = new PDO('mysql:host=127.0.0.1:dbname=tasks', 'debian-sys-maint', 'Sw8lPYpAFLi5B3n4');
-} catch (PDOException $e) {
-    die($e -> getMessage());
+//try {
+//    $pdo = new PDO('mysql:host=127.0.0.1:dbname=tasks', 'debian-sys-maint', 'Sw8lPYpAFLi5B3n4');
+//} catch (PDOException $e) {
+//    die($e -> getMessage());
+//};
+
+try{
+    $pdo = new PDO('mysql:host=127.0.0.1;dbname=tasks','debian-sys-maint','Sw8lPYpAFLi5B3n4');
+} catch (PDOException $e){
+    die($e->getMessage());
 };
+
 
 $query = $pdo->prepare('SELECT * FROM tasks');
 
